101d0a0ec2f7ef7fc504bd62530d03663819beb8
cwklausing <cwklausing@gmail.com>
2017-05-30 17:02:54 -0500

    beginning to work on state updates

--- js/Search.jsx
+++ js/Search.jsx
@@ -1,5 +1,49 @@
-import React from 'react';
+import React, { Component } from 'react';
+import preload from '../data.json';
+import ShowCard from './ShowCard';
 
-const Search = () => <h1>Hi this is my search</h1>;
+class Search extends Component {
+	state = {
+		searchTerm: ''
+	};
+
+	handleSearchTermChange = event => {
+		this.setState({ searchTerm: event.target.value });
+	};
+
+	render() {
+		return (
+			<div className="search">
+				<header>
+					<h1>ConFlix</h1>
+					<input
+						onChange={this.handleSearchTermChange}
+						value={this.state.searchTerm}
+						type="text"
+						placeholder="search"
+					/>
+				</header>
+				<div>
+					{preload.shows
+						.filter(
+							show =>
+								`${show.title} ${show.description}`
+									.toUpperCase()
+									.indexOf(this.state.searchTerm.toUpperCase()) >= 0
+						)
+						.map(show => (
+							<ShowCard
+								key={show.imdbID}
+								poster={show.poster}
+								title={show.title}
+								year={show.year}
+								description={show.description}
+							/>
+						))}
+				</div>
+			</div>
+		);
+	}
+}
 
 export default Search;
